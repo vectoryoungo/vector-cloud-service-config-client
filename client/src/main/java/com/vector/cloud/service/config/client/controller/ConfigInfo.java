473ad180997aa358c40badcfc082cebe2749ddb7
居洪涛 <hongtao.ju@blockchain001.com>
2019-08-16 17:16:18 +0800

    add initial config

---
+++ client/src/main/java/com/vector/cloud/service/config/client/controller/ConfigInfo.java
@@ -0,0 +1,24 @@
+/**
+ * @create 2019-08-16 16:13
+ * @desc read config center config info
+ **/
+package com.vector.cloud.service.config.client.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class ConfigInfo {
+
+    @Value("${vector.config.info}")
+    private String configInformation;
+
+
+    @GetMapping(value = "/getConfigInfo")
+    public String getConfigInformation() {
+        return configInformation;
+    }
+
+}
+
