1bb4be4d7a32433e71d74fac1432e124e6a564f1
居洪涛 <hongtao.ju@blockchain001.com>
2019-08-19 17:08:03 +0800

    add security

--- client/src/main/java/com/vector/cloud/service/config/client/controller/ConfigInfo.java
+++ client/src/main/java/com/vector/cloud/service/config/client/controller/ConfigInfo.java
@@ -5,10 +5,12 @@
 package com.vector.cloud.service.config.client.controller;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RefreshScope
 public class ConfigInfo {
 
     @Value("${vector.config.info}")
